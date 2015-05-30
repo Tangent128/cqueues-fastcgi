38fcf7bd3fc9209fd1b3228e5438eaa562a98db5
Tangent 128 <Tangent128@gmail.com>
2015-05-30 15:37:58 -0400

    * JSON web api test

--- test.lua
+++ test.lua
@@ -1,13 +1,26 @@
 
 local responder = require "cqueues-fastcgi.responder"
+local cjson = require "cjson"
 
 assert(responder.simpleLoop({
 	path = "fastcgi.socket",
 	unlink = true
 }, function(request)
+	local body = request:slurp()
+	local input = cjson.decode(body)
+	
 	request:header("Status", 200)
-	request:write "PARAMS:\n"
-	for k, v in pairs(request.params) do
+	request:write("CONTENT:\n")
+	--request:write(body)
+	for k, v in pairs(input) do
 		request:write(("%s = %s\n"):format(k, v))
 	end
+end, function(request, code, message)
+	request:header("Status", tostring(code).." "..message)
+	request:header("Content-Type", "application/json")
+	request:header("Encoding", "UTF-8")
+	request:write(cjson.encode {
+		status = code,
+		message = message
+	})
 end))
